1fe01e38ca50613ffee8ad3c7ca0c1ddeae0e992
kedarguy <kedarguy@gmail.com>
2017-06-06 11:49:54 +0300

    built displayMemeEditor first part

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'use strict'
 
 var gImgBank = [
-    {url: 'http://www.dumbdrops.com/ratchet-memes/templates/Will_Smith.jpg' ,desc: 'bigwill', id: 0, keywords:[]},
-    {url: 'https://s-media-cache-ak0.pinimg.com/originals/d4/ac/0b/d4ac0b96844e1d631cbf3e41227887da.jpg' ,desc: 'takemymoney', id: 1, keywords:[]},
-    {url: 'https://imgflip.com/s/meme/Imagination-Spongebob.jpg' ,desc: 'sponge', id: 2, keywords:[]},
-    {url: 'http://images.maariv.co.il/image/upload/f_auto,fl_lossy/t_ArticleControlMaarivTransformaionFaceDetect/442445' ,desc: 'orenhazan', id: 3, keywords:[]}
+    { url: 'http://www.dumbdrops.com/ratchet-memes/templates/Will_Smith.jpg', desc: 'bigwill', id: 0, keywords: [] },
+    { url: 'https://s-media-cache-ak0.pinimg.com/originals/d4/ac/0b/d4ac0b96844e1d631cbf3e41227887da.jpg', desc: 'takemymoney', id: 1, keywords: [] },
+    { url: 'https://imgflip.com/s/meme/Imagination-Spongebob.jpg', desc: 'sponge', id: 2, keywords: [] },
+    { url: 'http://images.maariv.co.il/image/upload/f_auto,fl_lossy/t_ArticleControlMaarivTransformaionFaceDetect/442445', desc: 'orenhazan', id: 3, keywords: [] }
     // {url: 'https://ca.slack-edge.com/T4Z64K5JM-U529YD0E9-deac2872f743-512' ,desc: 'rekeft', id: 4, keywords:[]},
     // {url: 'https://ca.slack-edge.com/T4Z64K5JM-U4ZCWULQH-5dba29cdc46d-512' ,desc: 'ilan', id: 5, keywords:[]},
     // {url: 'https://ca.slack-edge.com/T4Z64K5JM-U4ZU3UTEX-e7a6aa3e72cd-512' ,desc: 'erez', id: 6, keywords:[]},
@@ -18,16 +18,32 @@ var gImgBank = [
     // {url: 'https://goo.gl/photos/WxCs2rQVs3qskH3BA' ,desc: 'oren', id: 14, keywords:[]}
 ]
 
+var gElImgBoard;
+var gElEditContainer;
 
-var gImgBoard = document.querySelector('.imgs-container');
-renderImgs();
+function init() {
+    gElImgBoard = document.querySelector('.imgs-container');
+    gElEditContainer = document.querySelector('.edit-container');
+    
+    renderImgs();
+
+}
 
 function renderImgs() {
-    var strHtml = ''; 
-    for(var i=0; i<gImgBank.length; i++) {
-        strHtml += `<div class="single-img" onclick="memeCreator(this)">`;
-        strHtml += `<img src="${gImgBank[i].url}" alt="${gImgBank[i].desc}">`;
+    var strHtml = '';
+    for (var i = 0; i < gImgBank.length; i++) {
+        strHtml += `<div class="single-img" >`;
+        strHtml += `<img src="${gImgBank[i].url}" alt="${gImgBank[i].desc}" onclick="displayMemeEditor(this)">`;
         strHtml += `</div>`;
     }
-    gImgBoard.innerHTML = strHtml;
+    gElImgBoard.innerHTML = strHtml;
+}
+
+function displayMemeEditor(image) {
+    gElImgBoard.style.display = 'none';
+    gElEditContainer.style.display = 'flex';
+    var elMemeCanvas = document.querySelector('#memeCanvas');
+    var context = elMemeCanvas.getContext("2d");
+    context.drawImage(image, 0 , 0, elMemeCanvas.width, elMemeCanvas.height);
+     
 }
