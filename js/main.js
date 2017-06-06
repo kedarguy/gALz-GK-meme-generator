14ec983d6ffc2813ee0bcbf9d63b01a3d4a8a017
gALz <gal.important@gmail.com>
2017-06-06 14:48:49 +0300

    fix imgs

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 'use strict'
 
 var gImgBank = [
-    { url: 'http://www.dumbdrops.com/ratchet-memes/templates/Will_Smith.jpg', desc: 'bigwill', id: 0, keywords: [] },
-    { url: 'https://s-media-cache-ak0.pinimg.com/originals/d4/ac/0b/d4ac0b96844e1d631cbf3e41227887da.jpg', desc: 'takemymoney', id: 1, keywords: [] },
-    { url: 'https://imgflip.com/s/meme/Imagination-Spongebob.jpg', desc: 'sponge', id: 2, keywords: [] },
-    { url: 'http://images.maariv.co.il/image/upload/f_auto,fl_lossy/t_ArticleControlMaarivTransformaionFaceDetect/442445', desc: 'orenhazan', id: 3, keywords: [] },
-    {url: 'https://ca.slack-edge.com/T4Z64K5JM-U529YD0E9-deac2872f743-512' ,desc: 'rekeft', id: 4, keywords:[]},
-    {url: 'img/1.jpg' ,desc: 'ilan', id: 5, keywords:[]}
-    // {url: 'https://ca.slack-edge.com/T4Z64K5JM-U4ZU3UTEX-e7a6aa3e72cd-512' ,desc: 'erez', id: 6, keywords:[]},
-    // {url: 'https://ca.slack-edge.com/T4Z64K5JM-U508P149K-bd68ecba269e-512' ,desc: 'dor', id: 7, keywords:[]},
-    // {url: 'https://ca.slack-edge.com/T4Z64K5JM-U50JWF5PF-g8169be4ae78-512' ,desc: 'yaron', id: 8, keywords:[]},
-    // {url: 'https://ca.slack-edge.com/T4Z64K5JM-U50UEPNJH-g648a8e5dcf7-512' ,desc: 'tamir', id: 9, keywords:[]}
+    { url: 'assets/img/child.jpg', desc: 'child', id: 0, keywords: [] },
+    { url: 'assets/img/drevil.jpg', desc: 'drevil', id: 1, keywords: [] },
+    { url: 'assets/img/trump.jpg', desc: 'trump', id: 2, keywords: [] },
+    { url: 'assets/img/youdontsay.jpg', desc: 'youdontsay', id: 3, keywords: [] },
+    {url: 'assets/img/salt.png' ,desc: 'salt', id: 4, keywords:[]}
+    // {url: 'assets/img/rakef.png' ,desc: 'rakef', id: 5, keywords:[]}
+    // {url: 'assets/img/ilan.png' ,desc: 'ilan', id: 6, keywords:[]},
+    // {url: 'assets/img/dor.png' ,desc: 'dor', id: 7, keywords:[]},
+    // {url: 'assets/img/yaron.png' ,desc: 'yaron', id: 8, keywords:[]},
+    // {url: 'assets/img/tamir.png' ,desc: 'tamir', id: 9, keywords:[]}
     // {url: 'https://goo.gl/photos/WxCs2rQVs3qskH3BA' ,desc: 'yaron', id: 10, keywords:[]},
     // {url: 'https://goo.gl/photos/WxCs2rQVs3qskH3BA' ,desc: 'oren', id: 11, keywords:[]},
     // {url: 'https://goo.gl/photos/WxCs2rQVs3qskH3BA' ,desc: 'oren', id: 12, keywords:[]},
@@ -34,7 +34,7 @@ function renderImgs() {
     var strHtml = '';
     for (var i = 0; i < gImgBank.length; i++) {
         strHtml += `<div class="single-img" >`;
-        strHtml += `<img src="${gImgBank[i].url}" crossOrigin="Anonymous" alt="${gImgBank[i].desc}" onclick="displayMemeEditor(this)">`;
+        strHtml += `<img src="${gImgBank[i].url}" alt="${gImgBank[i].desc}" onclick="displayMemeEditor(this)">`;
         strHtml += `</div>`;
     }
     gElImgBoard.innerHTML = strHtml;
